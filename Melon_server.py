94eced8f326252f8a7e741ffb12756ab2e388d57
Shegzz12 <150711672+Shegzz12@users.noreply.github.com>
2023-12-01 13:49:43 -0800

    Add files via upload

--- Melon_server.py
+++ Melon_server.py
@@ -6,9 +6,9 @@ util_path = "util.py"
 app = Flask(__name__)
 
 # Initialize values to be used if no data is received
-val1 = 28.03
-val2 = 74.00
-val3 = 20.00
+val1 = 25.03
+val2 = 80.00
+val3 = 5.00
 
 @app.route('/input_data', methods=['POST'])
 def receive_data():
